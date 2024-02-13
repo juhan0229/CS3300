f059ff44c2fb296ac3644f71dccd710af15dac9e
juhan hong <juhan.hong0@gmail.com>
2024-02-13 00:21:17 -0700

    added url paths but does not work at the moment

--- portfolio/django_project/settings.py
+++ portfolio/django_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add the new app here
+    'portfolio.app'
 ]
 
 MIDDLEWARE = [
