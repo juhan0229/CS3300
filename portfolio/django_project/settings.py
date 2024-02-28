f3f57c8463ec689e9bbe904c0edb5b061c28c67c
juhan hong <juhan.hong0@gmail.com>
2024-02-27 20:54:07 -0700

    finished 2.4

--- portfolio/django_project/settings.py
+++ portfolio/django_project/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add your app name here
+    'portfolio_app',
+
 ]
+
+# Add support for authenticating users
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
